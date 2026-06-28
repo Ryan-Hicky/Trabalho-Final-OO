1f2c23c36e113907e98e897b0119a00d25ba4e6c
ailsonjvicente-lab <ailsonjvicente@gmail.com>
2026-06-28 17:30:32 -0300

    Create classe-Pedido.java

---
+++ classe-Pedido.java
@@ -0,0 +1,84 @@
+package br.edu.cafeteria.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import br.edu.cafeteria.excecao.EstoqueInsuficienteException;
+import br.edu.cafeteria.servico.Promocional;
+
+public class Pedido {
+
+    private static int contadorId;
+
+    private int idPedido;
+    private String atendente;
+    private Cliente cliente;
+    private List<ItemPedido> itens;
+    public Pedido(String atendente, Cliente cliente){
+        this.idPedido = contadorId++;
+        this.atendente = atendente;
+        this.cliente = cliente;
+        this.itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(Produto p) throws EstoqueInsuficienteException {
+        adicionarItem(p, 1);
+    }
+    public void adicionarItem(Produto p, int quantidde) throws EstoqueInsuficienteException{
+        if (p.QuantidadeEstoque() < quantidde){
+            throw new EstoqueInsuficienteException("Estoque insuficiente para o produto" + p.getNome());
+        }
+        ItemPedido novoItem = new ItemPedido(p, quantidde);
+        itens.add(novoItem);
+    }
+    public double calcularTotal(boolean diaDeEventoGeek){
+        double total = 0.0;
+
+        for (ItemPedido item : itens){
+            Produto produto = item.getProduto();
+            double precoItem = produto.getPrecoBase();
+            if (diaDeEventoGeek && produto instanceof Promocional){
+                Promocional promocional = (Promocional) produto;
+                precoItem = promocional.aplicarDesconto(precoItem);
+            }
+            total += precoItem * item.getQuantidade();
+        }
+        return total;
+    }
+    public void finalizarPedido(boolean diaDeEventoGeek, boolean resgatarXP) {
+        double totalFinal = calcularTotal(diaDeEventoGeek);
+
+        for (ItemPedido item: itens){
+            Produto p  = item.getProduto();
+            p.reduzirEstoque(item.getQuantidade());
+        }
+        if (cliente != null) {
+            if (resgatarXP){
+            }else{
+
+            }
+        }
+    }
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public String getAtendente() {
+        return atendente;
+    }
+
+    public void setAtendente(String atendente) {
+        this.atendente = atendente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+}
