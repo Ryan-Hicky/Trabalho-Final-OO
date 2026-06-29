58063fde2eafa3a2c3cfdcbc2a456dd3996b50c5
Ryan Hicky Alves de Meneses <hickymenesesbrgm@gmail.com>
2026-06-28 21:51:01 -0300

    Delete TrabalhoDeOO - Copia directory

--- TrabalhoDeOO - Copia/src/br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.cafeteria.excecao;
-
-// exceção checked
-public class EstoqueInsuficienteException extends Exception {
-    public EstoqueInsuficienteException(String mensagem) {
-        super(mensagem);
-    }
-}
--- TrabalhoDeOO - Copia/src/br/edu/cafeteria/excecao/PontosInsuficientesException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.cafeteria.excecao;
-
-// exceção checked
-public class PontosInsuficientesException extends Exception {
-    public PontosInsuficientesException(String mensagem) {
-        super(mensagem);
-    }
-}
